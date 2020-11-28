3c483184b5ef0b61bd64fdbffb63fcd7d09ecd36
hyunsungcho <hscho1226@gmail.com>
2020-11-29 03:06:21 +0900

    Update file structure

--- scooby_backend/scooby/views.py
+++ scooby_backend/scooby/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 from .models import Post
 from .serializers import PostSerializer
+from STT_models.stt_engine import MozillaSTT
+import os
+import scipy.io.wavfile
 
 # Create Views here
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
+
+class FileUploadView(APIView):
+    parser_class = (FileUploadParser,)
+
+    def put(self, request, format=None):
+        file_obj = request.FILES['file']
+        stt_result = handle_uploaded_file(file_obj)
+        print("Put response :" + stt_result)
+        return Response(data={"stt_result": stt_result}, status=status.HTTP_201_CREATED)
+
+def handle_uploaded_file(raw_audio):
+    # f is Cloass UploadedFile
+    # https://docs.djangoproject.com/en/3.1/ref/files/uploads/#django.core.files.uploadedfile.UploadedFile
+    # TODO: Transcribe
+    # Make this function in a separate file if needed
+
+    with open('myfile.wav', mode='bw') as f:
+        f.write(raw_audio.read())
+    stt_result = MozillaSTT('myfile.wav')# temporary
+
+    return stt_result
